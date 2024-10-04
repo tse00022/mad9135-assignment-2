1883c2498c6529f2d43d1684505e8f468a3ec210
Hiu Ming Tse <tse00022@algonquinlive.com>
2024-10-04 19:45:19 -0400

    Flatlish works

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from "react";
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import axios from "axios";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://random-data-api.com/api/users/random_user?size=10")
+      .then((response) => {
+        setUsers(response.data);
+      })
+      .catch((e) => {
+        alert("Network error, please try again later.");
+        console.error("Error fetching data: ", e);
+      });
+  }, []);
+
+  // Item renderer for FlatList
+  const renderItem = ({ item }) => {
+    console.log("item: ", item);
+    console.log("item.first_name: ", item.first_name);
+    return (<View>
+      <Text>{item.first_name}</Text>
+      <Text>{item.last_name}</Text>
+      <Text style={styles.body}>{item.uid}</Text>
+      <Text style={styles.body}>{item.avatar}</Text>
+    </View>)
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider style={styles.container}>
+      <SafeAreaView>
+        <Text>Hello</Text>
+        <FlatList
+          data={users}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 16,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
