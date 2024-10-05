bbec78e1cb4e4a347646df9c7f732271101a3bc1
Hiu Ming Tse <tse00022@algonquinlive.com>
2024-10-04 20:08:28 -0400

    Integrate with react-native-toast-message

--- App.js
+++ App.js
@@ -1,22 +1,38 @@
 import { useState, useEffect } from "react";
-import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { FlatList, SafeAreaView, StyleSheet, Text, View, RefreshControl } from "react-native";
 import axios from "axios";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import Toast from 'react-native-toast-message';
 
 export default function App() {
   const [users, setUsers] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
 
   useEffect(() => {
+    fetchUsers(10);
+  }, []);
+
+  const fetchUsers = (count = 1) => {
+    setRefreshing(true);
     axios
-      .get("https://random-data-api.com/api/users/random_user?size=10")
+      .get(`https://random-data-api.com/api/users/random_user?size=${count}`)
       .then((response) => {
         setUsers(response.data);
+        setRefreshing(false);
+        Toast.show({
+          type: 'success',
+          text1: 'Success',
+          text2: 'User list fetched successfully 👋',
+          position: 'bottom',
+          visibilityTime: 500,
+        });
       })
       .catch((e) => {
         alert("Network error, please try again later.");
         console.error("Error fetching data: ", e);
+        setRefreshing(false);
       });
-  }, []);
+  }
 
   // Item renderer for FlatList
   const renderItem = ({ item }) => {
@@ -33,12 +49,15 @@ export default function App() {
   return (
     <SafeAreaProvider style={styles.container}>
       <SafeAreaView>
-        <Text>Hello</Text>
         <FlatList
           data={users}
           renderItem={renderItem}
           keyExtractor={(item) => item.id.toString()}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={fetchUsers} />
+          }
         />
+        <Toast />
       </SafeAreaView>
     </SafeAreaProvider>
   );
